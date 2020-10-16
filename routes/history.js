6052c829cbdb8953cf7da079e506f3c74da62e67
Thomas Le Vot <totaotata@protonmail.com>
2020-10-16 14:20:25 +0200

    Panier OK

--- routes/history.js
+++ routes/history.js
@@ -2,8 +2,6 @@ var express = require("express");
 var router = express.Router();
 var HistoryModel = require("../models/histories");
 
-const { route } = require(".");
-
 router.get("/", function (req, res, next) {
   res.render("last-trip", { title: "Last trip" });
 });
